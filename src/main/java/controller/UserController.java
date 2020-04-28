11210a33cee7d06d375ef5e426516591fd4b13e3
shirleyndou <shirley.ndou@umuzi.org>
2020-04-28 04:53:47 +0200

    controller update

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import com.springapp.User;
+import com.springapp.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("api/v1/user")
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user.getId(), user.getName(), user.getSurname());
+    }
+
+    @GetMapping
+    public String getUser(@PathVariable("id") long Id){
+        return userService.getUser(Id);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void removeUser(@PathVariable("id") long Id){
+        userService.removeUser(Id);
+    }
+
+
 }
